fa49cbdba12180ac556469455a1ba66eabe14ec5
Bryan Mancilla <70993543@certus.edu.pe>
2023-12-12 19:11:17 -0500

    App principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http'
+import {NgbPaginationModule} from '@ng-bootstrap/ng-bootstrap'
+import {EditorModule} from '@tinymce/tinymce-angular'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { LoginComponent } from './components/login/login.component';
+import { IndexClienteComponent } from './components/clientes/index-cliente/index-cliente.component';
+import { CreateClienteComponent } from './components/clientes/create-cliente/create-cliente.component';
+import { EditClienteComponent } from './components/clientes/edit-cliente/edit-cliente.component';
+import { CreateProductoComponent } from './components/productos/create-producto/create-producto.component';
+import { IndexProductoComponent } from './components/productos/index-producto/index-producto.component';
+import { UpdateProductoComponent } from './components/productos/update-producto/update-producto.component';
+import { ConfigComponent } from './components/config/config.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    SidebarComponent,
+    LoginComponent,
+    IndexClienteComponent,
+    CreateClienteComponent,
+    EditClienteComponent,
+    CreateProductoComponent,
+    IndexProductoComponent,
+    UpdateProductoComponent,
+    ConfigComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    NgbPaginationModule,
+    EditorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
