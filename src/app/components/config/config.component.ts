d003453d3f64161ecfe13427af3174df201b2c25
Bryan Mancilla <70993543@certus.edu.pe>
2023-12-12 23:55:16 -0500

    Correción de errores en la configuración

--- src/app/components/config/config.component.ts
+++ src/app/components/config/config.component.ts
@@ -1,19 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { GLOBAL } from 'src/app/services/GLOBAL';
 import { AdminService } from 'src/app/services/admin.service';
 
 declare let iziToast: any;
-import { v4 as uuidv4 } from 'uuid'
+import { v4 as uuidv4 } from 'uuid';
 
 @Component({
   selector: 'app-config',
   templateUrl: './config.component.html',
-  styleUrls: ['./config.component.css']
+  styleUrls: ['./config.component.css'],
 })
 export class ConfigComponent implements OnInit {
-
   public token;
-  public config : any = {}; 
+  public config: any = {};
 
   public titulo_cat = '';
   public icono_cat = '';
@@ -21,86 +21,95 @@ export class ConfigComponent implements OnInit {
 
   public imgSelect: any | ArrayBuffer;
   public fileName: string = 'Seleccionar imagen';
+  public url: any;
 
-  constructor(
-    private _adminService: AdminService
-  ){
-    this.token = localStorage.getItem('token')
+  constructor(private _adminService: AdminService) {
+    this.token = localStorage.getItem('token');
     
-    this._adminService.obtener_config_admin(this.token).subscribe({
-      next: response => {
-       /*  console.log(response); */
-       this.config = response.data
-        console.log(this.config);
-        
-        
-      },
-      error: error => {
-        console.log(error);
-        
-      }
-    })
-  }
-  ngOnInit(): void {
-  }
 
-  agregar_cat(){
+    this._adminService.obtener_config_admin(this.token).subscribe({
+      next: (response) => {
+        /*  console.log(response); */
+        this.config = response.data;
+        console.log(this.config);
+
+        this.url = GLOBAL.url + 'obtener_logo/' + this.config.logo; // Corregir la URL
+
+        // También podrías establecer la imagen seleccionada aquí si es necesario
+        this.imgSelect = this.url;
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
+  ngOnInit(): void {}
+
+  agregar_cat() {
     if (this.titulo_cat && this.icono_cat) {
       console.log(uuidv4());
-      
+
       this.config.categorias.push({
         titulo: this.titulo_cat,
         icono: this.icono_cat,
-        _id: uuidv4()
-      })
+        _id: uuidv4(),
+      });
 
       this.titulo_cat = '';
       this.icono_cat = '';
-    }else{
+    } else {
       iziToast.show({
         title: 'ERROR',
         titleColor: '#FF0000',
         color: '#FFF',
         class: 'text-danger',
         position: 'topRight',
-        message: 'Debe ingresar un titulo y un icono para la categoria'
+        message: 'Debe ingresar un titulo y un icono para la categoria',
       });
     }
   }
-  actualizar(confForm: NgForm){
+  actualizar(confForm: NgForm) {
     if (confForm.valid) {
       let data = {
         titulo: confForm.value.titulo,
         serie: confForm.value.serie,
         correlativo: confForm.value.correlativo,
         categorias: this.config.categorias,
-        logo: this.file
-      }
+        logo: this.file,
+      };
       console.log(data);
-      this._adminService.actualiza_config_admin("657782fedd5f2bebac36c858",data, this.token).subscribe({
-        next: response => {
-          console.log(response);
-          
-        },
-        error: error => {
-          console.log(error);
-          
-        }
-      })
-      
-    }else{
+      this._adminService
+        .actualiza_config_admin('657782fedd5f2bebac36c858', data, this.token)
+        .subscribe({
+          next: (response) => {
+            console.log(response);
+            iziToast.show({
+              title: 'Success',
+              titleColor: '#1DC74C',
+              color: '#FFF',
+              class: 'text-success',
+              position: 'topRight',
+              message: 'Se actualizó correctamente la configuración.',
+            });
+  
+          },
+          error: (error) => {
+            console.log(error);
+          },
+        });
+    } else {
       iziToast.show({
         title: 'ERROR',
         titleColor: '#FF0000',
         color: '#FFF',
         class: 'text-danger',
         position: 'topRight',
-        message: 'Complete correctamente el formulario'
+        message: 'Complete correctamente el formulario',
       });
     }
   }
 
-  fileChangeEvent(fileInput: HTMLInputElement){
+  fileChangeEvent(fileInput: HTMLInputElement) {
     if (fileInput.files && fileInput.files[0]) {
       this.file = fileInput.files[0];
       console.log(this.file);
@@ -129,8 +138,7 @@ export class ConfigComponent implements OnInit {
       ) {
         const reader = new FileReader();
         reader.onload = (e) => (this.imgSelect = reader.result);
-        reader.readAsDataURL(this.file)
-
+        reader.readAsDataURL(this.file);
       } else {
         iziToast.show({
           title: 'ERROR',
@@ -158,6 +166,8 @@ export class ConfigComponent implements OnInit {
 
     console.log(this.file);
   }
-  
-}
 
+  eliminar_categoria(idx:any){
+    this.config.categorias.splice(idx, 1)
+  }
+}
